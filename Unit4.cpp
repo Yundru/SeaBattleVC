975df2870d70b3dada48499215dab15e1f3e2c17
Yundru <Klavakor02@gmail.com>
2026-06-26 10:19:15 +0800

    Версия_3

--- Unit4.cpp
+++ Unit4.cpp
@@ -21,18 +21,18 @@ __fastcall TForm4::TForm4(TComponent* Owner)
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TForm4::PaintBox1MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+void __fastcall TForm4::PaintBox2MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
           int X, int Y)
 {
 		   A=X/50; B=Y/50;
 		   if (C==0 && A<10 && B<10 && !pozvistrplayer[A][B]) {
 
 
-		   PaintBox1->Canvas->MoveTo(A*50+1,B*50+1);
-		   PaintBox1->Canvas->LineTo(A*50+49,B*50+49);
+		   PaintBox2->Canvas->MoveTo(A*50+1,B*50+1);
+		   PaintBox2->Canvas->LineTo(A*50+49,B*50+49);
 
-		   PaintBox1->Canvas->MoveTo(A*50+49,B*50+1);
-		   PaintBox1->Canvas->LineTo(A*50+1,B*50+49);
+		   PaintBox2->Canvas->MoveTo(A*50+49,B*50+1);
+		   PaintBox2->Canvas->LineTo(A*50+1,B*50+49);
 		   pozvistrplayer[A][B] = true;
 		   C=1;
 		   }
@@ -40,27 +40,27 @@ void __fastcall TForm4::PaintBox1MouseDown(TObject *Sender, TMouseButton Button,
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TForm4::PaintBox1MouseUp(TObject *Sender, TMouseButton Button, TShiftState Shift,
+void __fastcall TForm4::PaintBox2MouseUp(TObject *Sender, TMouseButton Button, TShiftState Shift,
           int X, int Y)
-{       if(C==1)
+{
+		if(C==1)
 		{
 			srand(time(nullptr));
 			int x, y;
 			do
 			{
-				x = (rand() % (PaintBox2->Width + 1)) / 50;
-				y = (rand() % (PaintBox2->Height + 1)) / 50;
+				x = (rand() % (500 + 1)) / 50;
+				y = (rand() % (500 + 1)) / 50;
 			}while (pozvistrrobot[x][y] || x >10 || y >10);
 
-		   PaintBox2->Canvas->MoveTo(x*50+1,y*50+1);
-		   PaintBox2->Canvas->LineTo(x*50+49,y*50+49);
+		   PaintBox1->Canvas->MoveTo(x*50+1,y*50+1);
+		   PaintBox1->Canvas->LineTo(x*50+49,y*50+49);
 
-		   PaintBox2->Canvas->MoveTo(x*50+49,y*50+1);
-		   PaintBox2->Canvas->LineTo(x*50+1,y*50+49);
+		   PaintBox1->Canvas->MoveTo(x*50+49,y*50+1);
+		   PaintBox1->Canvas->LineTo(x*50+1,y*50+49);
 		   pozvistrrobot[x][y] = true;
 		   C=0;
 		}
 }
 //---------------------------------------------------------------------------
-
 
